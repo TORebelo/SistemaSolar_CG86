43c221fc01523308738bef9a9c932729d788747e
TORebelo <torbuntu@DESKTOP-Q5GL3SU.>
2024-11-16 06:30:25 +0100

    astros/esferas

---
+++ project/src/Objs/astro.js
@@ -0,0 +1,98 @@
+import VisualObject from './abstract'
+import { m4, primitives } from '../../lib/twgl-full'
+
+
+/**
+ * Sphere Class
+ */
+class Astro extends VisualObject {
+    static vs = `
+    uniform mat4 u_worldViewProjection;
+    uniform mat4 u_transform;
+    attribute vec4 position;
+    attribute vec3 normal;
+    
+    varying vec4 norm;
+    
+    void main() {
+        gl_Position = u_worldViewProjection * (position * u_transform);
+        norm = vec4(normal, 1);
+    }`
+
+    static fs = `
+    precision mediump float;
+    
+    varying vec4 norm;
+    
+    void main() {
+        gl_FragColor = norm * vec4(1, 1, 1, 1);
+    }`
+
+    constructor(gl, radius, divs, parent) {
+        super(gl, parent)
+        this.bufferInfo = primitives.createSphereBufferInfo(gl, radius, divs, divs)
+
+        this.transformations = []
+
+    }
+
+    update(time) {
+        super.update()
+
+        let totalTrans = m4.identity();
+
+        for (var i = this.transformations.length - 1; i >= 0; i--) {
+            totalTrans = m4.multiply(this.transformations[i](time), totalTrans)
+        }
+
+
+        this.uniforms.u_transform = totalTrans
+
+
+    }
+
+    render(gl, time) {
+        super.render(gl, time)
+    }
+
+    addTranslation(x, y, z) {
+        this.transformations.push((time) => {
+            return [
+                1, 0, 0, x,
+                0, 1, 0, y,
+                0, 0, 1, z,
+                0, 0, 0, 1
+            ]
+        });
+    }
+
+    addRotationX(alpha, staticRotation = false) {
+        this.transformations.push((time) => { return m4.rotationX(degreesToRad(alpha) * (staticRotation ? 1 : time)) });
+    }
+
+    addRotationY(alpha, staticRotation = false) {
+        this.transformations.push((time) => { return m4.rotationY(degreesToRad(alpha) * (staticRotation ? 1 : time)) });
+    }
+
+    addRotationZ(alpha, staticRotation = false) {
+        this.transformations.push((time) => { return m4.rotationZ(degreesToRad(alpha) * (staticRotation ? 1 : time)) });
+    }
+
+    addEllipseZ(a, b, alpha) {
+        this.transformations.push((time) => {
+            return [
+                1, 0, 0, a * Math.cos(alpha * time),
+                0, 1, 0, b * Math.sin(alpha * time),
+                0, 0, 1, 0,
+                0, 0, 0, 1
+            ]
+        });
+    }
+
+}
+
+const degreesToRad = deg => {
+    return deg * Math.PI / 180;
+}
+
+export default Sphere
--- project/src/main.js
+++ project/src/main.js
@@ -1,2 +1,39 @@
-import * as twlg from '../lib/twlg-full'
-import planeta from './Objs/planeta'
+import * as twgl from '../lib/twgl-full'
+import Astro from './Objs/astro'
+
+
+
+let initwebgl = () => {
+    const canvas = document.querySelector("canvas");
+    const gl = canvas.getContext("webgl");
+    if (!gl) {
+        throw 'webgl not supported';
+    }
+
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+
+    return gl;
+}
+
+
+
+/**
+ * Initializes the WebGL context, creates a astro object, and starts the render loop.
+ * The render loop updates and renders the astro at each frame.
+ */
+let main = () => {
+    let gl = initwebgl()
+    let astro = new Astro(gl, 1, 24)
+    let time = 0
+
+    let render = () => {
+        time += 0.01
+        astro.update(time)
+        astro.render(gl, time)
+        requestAnimationFrame(render)
+    }
+
+    render()
+}
+
